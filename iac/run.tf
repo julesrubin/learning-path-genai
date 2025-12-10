41d98852e8c7f115db05cdf7618d6f10a7140e77
Jules Rubin <jules.rubin.jr@gmail.com>
2025-12-11 00:01:13 +0100

    feat(iac): define environment variables for Cloud Run service configuration

--- iac/run.tf
+++ iac/run.tf
@@ -1,24 +1,43 @@
-# locals {
-#   services = {
-#     genai_api = {
-#       service_name = "genai-api"
-#       service_port = 8080
-#     }
-#   }
-# }
+locals {
+  services = {
+    genai_api = {
+      service_name = "genai-api"
+      service_port = 8080
+      env_vars = {
+        "GOOGLE_CLOUD_PROJECT"  = var.project_id
+        "GOOGLE_CLOUD_LOCATION" = var.region
+      }
+    }
+  }
+}
 
-# resource "google_cloud_run_v2_service" "default" {
-#   for_each = local.services
+resource "google_cloud_run_v2_service" "default" {
+  for_each = local.services
 
-#   project  = var.project_id
-#   location = var.region
-#   name     = each.value.service_name
-#   template {
-#     containers {
-#       image = "${google_artifact_registry_repository.learning_path_genai.name}/${each.value.service_name}:latest"
-#       ports {
-#         container_port = each.value.service_port
-#       }
-#     }
-#   }
-# }
+  project  = var.project_id
+  location = var.region
+  name     = each.value.service_name
+
+  template {
+    service_account = google_service_account.cloud_run.email
+
+    containers {
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-learning-path-genai/${each.value.service_name}:latest"
+
+      ports {
+        container_port = each.value.service_port
+      }
+
+      dynamic "env" {
+        for_each = each.value.env_vars
+        content {
+          name  = env.key
+          value = env.value
+        }
+      }
+    }
+  }
+
+  # Ensure the image exists before creating the service
+  depends_on = [google_artifact_registry_repository.learning_path_genai]
+}
